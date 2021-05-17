898403bffc213ccb4a21315ffc6bdfea755e2e6a
yesol-Han <55419946+yesol-Han@users.noreply.github.com>
2021-05-18 03:23:06 +0900

    Update 해시-전화번호 목록.cpp

--- C++/Level 2/해시-전화번호 목록.cpp
+++ C++/Level 2/해시-전화번호 목록.cpp
@@ -1,3 +1,28 @@
+// 2차 시도 - 정답
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    int length = phone_book.size();
+    
+    sort(phone_book.begin(),phone_book.end());
+    
+    for (int i=0; i<length-1 ; i++){
+        if(phone_book[i] == phone_book[i+1].substr(0,phone_book[i].size()))
+            return answer=false;
+    }
+    
+    return answer;
+}
+
+
+///////////////////////////////////////////////////
+
+// 1차 시도- 효율성 3,4 불통
+
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -14,7 +39,6 @@ bool solution(vector<string> phone_book) {
         for(int j=i; j<length ; j++){
             if(i==j) continue;
             if(phone_book[i][0] != phone_book[j][0]) break;
-            //cout<<phone_book[i]<<"-"<<phone_book[j]+"* ";
             
             if(comp < phone_book[j].size() & phone_book[j].find(phone_book[i]) == 0){
                 answer = false;
