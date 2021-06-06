5bcc2c0bd8d27472e41b6beb6f41456a4bb0804b
yesol-Han <55419946+yesol-Han@users.noreply.github.com>
2021-06-06 16:06:38 +0900

    Create 힙 - 더 맵게.py

---
+++ Python/Level 2/힙 - 더 맵게.py
@@ -0,0 +1,27 @@
+# 1차 시도
+# 정확성: 76.2
+# 효율성: 0.0
+# 합계: 76.2 / 100.0
+
+def solution(scoville, K):
+    answer = 0
+    
+    scoville.sort()
+    
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            answer = -1
+            return answer
+        
+        scoville.sort()
+        
+        new_sco = scoville[0] + scoville[1] * 2
+        del scoville[0:2]
+        scoville.append(new_sco)
+        
+        answer = answer+1
+    
+    print(scoville)
+    return answer 
+  
+  
