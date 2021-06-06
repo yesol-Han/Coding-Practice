e37c93a8ab7ec6f2beacdb9f17a91e71bfd67e3c
yesol-Han <55419946+yesol-Han@users.noreply.github.com>
2021-06-06 16:13:59 +0900

    Update 힙 - 더 맵게.py

--- Python/Level 2/힙 - 더 맵게.py
+++ Python/Level 2/힙 - 더 맵게.py
@@ -1,3 +1,28 @@
+# 2차 시도 (정답)
+# 정확성: 76.2
+# 효율성: 23.8
+# 합계: 100.0 / 100.0
+    
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            answer = -1
+            return answer
+        
+        new_sco = heapq.heappop(scoville) + heapq.heappop(scoville)*2
+        heapq.heappush(scoville, new_sco)
+        
+        answer = answer+1
+    
+    print(scoville)
+    return answer
+
 # 1차 시도
 # 정확성: 76.2
 # 효율성: 0.0
